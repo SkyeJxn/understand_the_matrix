7ca9ec0ef39b1d682fb85344fc2dd0e6e1a05652
Charlotte <ch.holle@gmx.de>
2025-11-13 14:51:09 +0100

    chore: integrate the component TutorialLevel

--- src/app/[mode]/[id]/page.js
+++ src/app/[mode]/[id]/page.js
@@ -1,8 +1,11 @@
+import { TutorialLevel } from "@/components/TutorialLevel";
+
 export default async function LevelPage({ params }) {
   const { mode, id } = await params;
   return (
     <div>
       <div className="header">{mode} - Level {id}</div>
+      {mode === 'tutorial' && (<TutorialLevel level_id={id}></TutorialLevel>)}
     </div>
   );
 }
