784e7cc9a898fab82aabd30d203c95d571df9380
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-11-24 13:47:29 +0100

    fix: remove await on params, force dynamic routing for [mode]/[id]

--- Webservice/src/app/[mode]/[id]/page.js
+++ Webservice/src/app/[mode]/[id]/page.js
@@ -2,13 +2,14 @@ import { TutorialLevel } from "@/components/TutorialLevel";
 import Gauss from "@/components/Level_2_gauss";
 
 export default async function LevelPage({ params }) {
-  const { mode, id } = await params;
+  const { mode, id } = params;
   return (
     <div>
       <div className="header">{mode} - Level {id}</div>
-      {mode === 'tutorial' && (<TutorialLevel level_id={id}></TutorialLevel>)}
+      {mode === 'tutorial' && (<TutorialLevel level_id={id}/>)}
       {mode === 'challenge' && id == 2 && (<Gauss />) }
     </div>
   );
 }
 
+export const dynamic = 'force-dynamic';
