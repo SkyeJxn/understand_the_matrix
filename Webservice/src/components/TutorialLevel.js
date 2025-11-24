9264c395234690f416bb4b3725cdb8009293180b
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-11-24 14:10:24 +0100

    fix: change level data import to fetch

--- Webservice/src/components/TutorialLevel.js
+++ Webservice/src/components/TutorialLevel.js
@@ -1,9 +1,8 @@
 "use client";
 import './TutorialLevel.css'
-import tutorial_data from "../data/tutorial_data.json"
 import 'katex/dist/katex.min.css';
 import { InlineMath } from 'react-katex';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "primereact/resources/themes/nano/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -12,20 +11,29 @@ import {LevelEndContent, NavigationArrows, Toolbar} from './LevelTools';
 
 
 export function TutorialLevel({ level_id = "1" }) {
+
+  const [tutorialData, setTutorialData] = UseState([]);
+
+  useEffect(() => {
+    fetch("/data/tutorial_data.json")
+    .then(res => res.json())
+    .then(data => setTutorialData(data))
+  })
+
   const [page, setPage] = useState("1");
   const [currentPart, setCurrentPart] = useState(1);
   const [progressValue, setProgressValue] = useState(0);
 
   // number of parts in the level
   const partsOnLevel = Math.max(
-    ...tutorial_data
+    ...tutorialData
       .filter(e => e.id === level_id)
       .map(e => Number(e.part))
   );
 
   // number of parts on a page
   function getMaxPart(page) {
-    const partsOnPage = tutorial_data
+    const partsOnPage = tutorialData
       .filter(row => row.id === level_id && row.page === page)
       .map(row => Number(row.part));
     return Math.max(...partsOnPage);
@@ -53,7 +61,7 @@ export function TutorialLevel({ level_id = "1" }) {
   }
 
   function nextLevel(){
-    const exists = tutorial_data.some(row => row.id == (Number(level_id)+1));
+    const exists = tutorialData.some(row => row.id == (Number(level_id)+1));
     if(exists) return (Number(level_id)+1);
     return null;
   }
@@ -77,7 +85,7 @@ export function TutorialLevel({ level_id = "1" }) {
 import React from 'react';
 function Content({ id, page, part }) {
   // all parts to the current part
-  const data = tutorial_data.filter(
+  const data = tutorialData.filter(
     row => row.id === id && row.page === page && Number(row.part) <= part
   );
 
