58e39a321adb3ca168a461f8fd26fe453b4e64c2
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-12-03 12:57:59 +0100

    feat: add file-system API endpoint

---
+++ Webservice/src/app/api/file-system/route.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function GET(request) {
+    const {searchParams}  = new URL(request.url);
+    const mode = searchParams.get('mode');
+    const level_id = searchParams.get('level_id');
+    const dir = path.join(process.cwd(), 'public', 'data', mode);
+
+    try{
+        const files = fs.readdirSync(dir);
+        const targetFile = files.find(
+            (file) => file.startsWith(`${level_id}- `) && file.endsWith(".json")
+        );
+        if (!targetFile) {
+            return new Response(JSON.stringify({ error: "File not found"}), {status: 404});
+        }
+        return new Response(JSON.stringify({filename: targetFile}), {status: 200});
+    } catch (err) {
+        return new Response(JSON.stringify({ error: err.message }), {status: 500});
+    }
+}
