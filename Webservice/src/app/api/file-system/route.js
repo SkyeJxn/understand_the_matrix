19826c0774f16da5d5da39212623b440f2a63e25
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2025-12-03 20:22:59 +0100

    refactor: update API to return full filename

--- Webservice/src/app/api/file-system/route.js
+++ Webservice/src/app/api/file-system/route.js
@@ -10,12 +10,12 @@ export async function GET(request) {
     try{
         const files = fs.readdirSync(dir);
         const targetFile = files.find(
-            (file) => file.startsWith(`${level_id}- `) && file.endsWith(".json")
+            (file) => file.startsWith(`${level_id}-`) && file.endsWith(".json")
         );
         if (!targetFile) {
             return new Response(JSON.stringify({ error: "File not found"}), {status: 404});
         }
-        return new Response(JSON.stringify({filename: targetFile}), {status: 200});
+        return new Response(JSON.stringify({filename: `/data/${mode}/${targetFile}`}), {status: 200});
     } catch (err) {
         return new Response(JSON.stringify({ error: err.message }), {status: 500});
     }
