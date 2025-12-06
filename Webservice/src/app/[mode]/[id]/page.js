ad9ad0aae34abf0b47eccf9a4dddc4660b7ddce0
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-12-06 23:03:56 +0100

    refactor: add ChallengeLevel Call

--- Webservice/src/app/[mode]/[id]/page.js
+++ Webservice/src/app/[mode]/[id]/page.js
@@ -2,6 +2,7 @@ import { TutorialLevel } from "@/components/TutorialLevel";
 import Gauss from "@/components/Level_2_gauss";
 import Header from "@/components/Header";
 import { Badge } from "primereact/badge";
+import { ChallengeLevel } from "@/components/ChallengeLevel";
 
 export const dynamic = "force-dynamic";
 
@@ -11,7 +12,9 @@ export default async function LevelPage({ params }) {
     <div style={{height: '100vh',display: 'flex',flexDirection: 'column', overflow: 'hidden'}}>
       <Header>{mode}<Badge value={id} style={{background: 'var(--color4)',}}/></Header>
       {mode === 'tutorial' && (<TutorialLevel level_id={id}/>)}
+      {mode === 'challenge' && (<ChallengeLevel level_id={id}/>)}
       {mode === 'challenge' && id == 2 && (<Gauss />) }
+
     </div>
   );
 }
