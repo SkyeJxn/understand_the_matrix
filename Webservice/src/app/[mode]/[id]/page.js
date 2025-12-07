7de7dd8fbf33913284692ed03c019170a0fdece5
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-12-07 20:03:36 +0100

    refactor: implement new rendering component

--- Webservice/src/app/[mode]/[id]/page.js
+++ Webservice/src/app/[mode]/[id]/page.js
@@ -1,8 +1,6 @@
-import { TutorialLevel } from "@/components/TutorialLevel";
-import Gauss from "@/components/Level_2_gauss";
 import Header from "@/components/Header";
 import { Badge } from "primereact/badge";
-import { ChallengeLevel } from "@/components/ChallengeLevel";
+import { LevelRenderer } from "@/components/LevelRenderer";
 
 export const dynamic = "force-dynamic";
 
@@ -11,10 +9,7 @@ export default async function LevelPage({ params }) {
   return (
     <div style={{height: '100vh',display: 'flex',flexDirection: 'column', overflow: 'hidden'}}>
       <Header>{mode}<Badge value={id} style={{background: 'var(--color4)',}}/></Header>
-      {mode === 'tutorial' && (<TutorialLevel level_id={id}/>)}
-      {mode === 'challenge' && (<ChallengeLevel level_id={id}/>)}
-      {mode === 'challenge' && id == 2 && (<Gauss />) }
-
+      <LevelRenderer mode={mode} level_id={id}/>
     </div>
   );
 }
