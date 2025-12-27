9ddf03555f45b09c386a9fe59ea03aa94b4def8d
Charlotte <ch.holle@gmx.de>
2025-12-27 17:28:34 +0100

    fix: add missing dependency array to useEffect

--- Webservice/src/app.jsx
+++ Webservice/src/app.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     document.body.classList.add("theme-green");
-  })
+  },[]);
   
   return (
     <Routes>
