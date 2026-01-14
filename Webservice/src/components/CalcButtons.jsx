b29d64e7d95debfba26accca2f4b2289cf18488a
Charlotte <ch.holle@gmx.de>
2026-01-14 21:37:25 +0100

    refactor: CalcButtons instead of Dialog with inputs and Dropdowns

--- Webservice/src/components/CalcButtons.jsx
+++ Webservice/src/components/CalcButtons.jsx
@@ -1,11 +1,10 @@
 import '../styles/CalcButtons.css'
-import React, {useState} from "react";
+import {useState} from "react";
 import { fraction } from "mathjs";
 import { Button } from "primereact/button";
-import { Dialog } from "primereact/dialog";
-import { SelectButton } from "primereact/selectbutton";
 import { InlineMath } from "react-katex";
 import { SwitchRows, MultiplyRow, AddRows } from "../utilities/CalcFunctions";
+import { Dropdown } from 'primereact/dropdown';
 
 /** 
  * Component that displays Calculation function buttons in challenges
@@ -18,24 +17,55 @@ import { SwitchRows, MultiplyRow, AddRows } from "../utilities/CalcFunctions";
  * @returns {JSX.Element}
  */
 export function CalcButtons({DisableZV = false, DisableZA = false, DisableZM = false, matrix, setMatrix}) {
+  const [mode, setMode] = useState(null); 
+  // mode = "mult" | "add" | "switch" | null
   if (!Array.isArray(matrix)) return <></>;
   if(matrix.length === 0) return <></>;
   return (
-    <div className="calc_btns" >
-        <MultButton deactivate={DisableZM} matrix={matrix} setMatrix={setMatrix} />
-        <AddButton deactivate={DisableZA} matrix={matrix} setMatrix={setMatrix} />
-        <SwitchButton deactivate={DisableZV} matrix={matrix} setMatrix={setMatrix} />
-    </div>
+    <>
+      {mode === null ? (
+        <div className="calc_btns">
+          <Button
+            disabled={DisableZM}
+            onClick={() => { setMode("mult") }}>
+            <InlineMath math="\xrightarrow{\rm{ZM}_{i} (S)}" />
+          </Button>
+          <Button
+            disabled={DisableZA}
+            onClick={() => {setMode("add")}}>
+            <InlineMath math="\xrightarrow{\rm{ZA}_{ij} (S)}" />
+          </Button>
+          <Button
+            disabled={DisableZV}
+            onClick={() => {setMode('switch')}}>
+            <InlineMath math="\xrightarrow{\rm{ZV}_{ij}}" />
+          </Button>
+        </div>
+      ):(
+      <>
+      
+        {mode === "mult" && ( 
+          <MultInline matrix={matrix} setMatrix={setMatrix} onClose={() => setMode(null)} /> 
+        )} 
+        {mode === "add" && ( 
+          <AddInline matrix={matrix} setMatrix={setMatrix} onClose={() => setMode(null)} /> 
+        )} 
+        {mode === "switch" && ( 
+          <SwitchInline matrix={matrix} setMatrix={setMatrix} onClose={() => setMode(null)} /> 
+        )}
+      </>
+      )}
+
+    </>
   );
 }
 
-function MultButton({ deactivate, matrix, setMatrix }){
+function MultInline({ matrix, setMatrix, onClose }){
   const dimension = matrix.length;
   const items = Array.from({ length: dimension }, (_, i) => ({
         label: `${i+1}`, value: i }
   ));
 
-    const [visible, setVisible] = useState(false);
     const [rowValue, setRowValue] = useState(items[0].value);
     const [scalarInput, setScalarInput] = useState("1");
     const [scalarFeedback, setScalarFeedback] = useState('1');
@@ -44,7 +74,7 @@ function MultButton({ deactivate, matrix, setMatrix }){
     function onConfirm(){
       const newMatrix = MultiplyRow(matrix, rowValue, scalar);
       setMatrix(newMatrix);
-      setVisible(false);
+      onClose();
     }
     function handleInput(input){
       setScalarInput(input);
@@ -70,59 +100,57 @@ function MultButton({ deactivate, matrix, setMatrix }){
       catch { setScalarFeedback('invalid')}
     }
 
-    const dialogFooter = (
-        <div className='dialog_footer'>
-            <Button label="Apply" icon="pi pi-check" onClick={onConfirm}/>
-        </div>
-    )
+    return(
+      <>
+  <div className="arrow-wrapper">
+    <div className="arrow-expression">
+      ZM
+      <sub>
 
-  return (
-    <div>
-      <Button
-        disabled={deactivate}
-        onClick={() => { setVisible(true); }}>
-        <InlineMath math="\xrightarrow{\rm{ZM}_{i} (S)}" />
-      </Button>
+        <Dropdown value={rowValue} 
+                  onChange={(e) => setRowValue(e.value)} 
+                  options={items}
+                  placeholder="Row" className="expr-input" />
 
-      <Dialog
-        className='dialog'
-        header="Multiply Row"
-        visible={visible}
-        onHide={() => { if (!visible) return; setVisible(false); }}
-        footer={dialogFooter}>
-          <div style={{marginTop: "10px"}}>
-            <InlineMath math={`\\LARGE \\xrightarrow{\\rm{ZM}_{${rowValue +1}} (${scalarFeedback})}`} />
-          </div>
-          <table className='dialog_table'><thead>
-          <tr>
-              <th>Row i</th>
-              <th>Scalar</th>
-          </tr>
-          <tr className="row_container">
-            <td>
-              <SelectButton className="select_btn" value={rowValue} onChange={(e) => setRowValue(e.value)} options={items}/>
-            </td>
-            <td>
-            <input className="scalar_input" 
-            type='text'
-            value={scalarInput}   
-            onChange={(e) => { handleInput(e.target.value)}} 
-            />
-            </td>
-          </tr>
-        </thead></table>
-      </Dialog>
+      </sub>
+      (
+      <input className="scalar_input" 
+              type='text' 
+              value={scalarInput}
+              onChange={(e) => { handleInput(e.target.value)}}
+              style={{border: scalarFeedback === 'invalid' ? '3px solid red' : '3px solid var(--color5)'}}
+              />
+      )
+
     </div>
-  );
+
+    {/* arrow */}
+    <div className="arrow-svg">
+    <svg className="arrow-svg-inner" viewBox="0 0 100 20" preserveAspectRatio="none">
+      <line x1="0" y1="10" x2="95" y2="10" className="arrow-line" />
+      <polygon points="95,5 100,10 95,15" className="arrow-head" />
+    </svg>
+  </div>
+
+  </div>
+
+  <div className='dialog_footer'>
+      <Button label="Cancel" icon="pi pi-times" onClick={() => onClose()}/>
+      <Button label="Apply" icon="pi pi-check" onClick={onConfirm} disabled={scalarFeedback === 'invalid'}/>
+  </div>
+
+
+</>
+);
+
 }
 
-function AddButton({ deactivate, matrix, setMatrix }) {
+function AddInline({ matrix, setMatrix, onClose }) {
   const dimension = matrix.length;
     const items = Array.from({ length: dimension }, (_, i) => ({
       label: `${i+1}`, value: i }
     ));
 
-    const [visible, setVisible] = useState(false);
     const [sourceValue, setSourceValue] = useState(items[0].value);
     const [targetValue, setTargetValue] = useState(items[0].value);
     const [scalarInput, setScalarInput] = useState("1");
@@ -132,7 +160,7 @@ function AddButton({ deactivate, matrix, setMatrix }) {
     function onConfirm() {
       const newMatrix = AddRows(matrix, sourceValue, targetValue, scalar);
       setMatrix(newMatrix);
-      setVisible(false);
+      onClose();
     }
 
     function handleInput(input){
@@ -159,151 +187,105 @@ function AddButton({ deactivate, matrix, setMatrix }) {
       catch { setScalarFeedback('invalid')}
     }
 
-    const dialogFooter = (
-       <div className='dialog_footer'>
-         <Button label="Apply" icon="pi pi-check" onClick={onConfirm} />
-       </div>
-    );
+    return(
+      <>
+  <div className="arrow-wrapper">
+    <div className="arrow-expression">
+      ZA
+      <sub>
 
-    return (
-      <div>
-        <Button
-          disabled={deactivate}
-          onClick={() => {
-            setVisible(true);
-          }}
-        >
-          <InlineMath math="\xrightarrow{\rm{ZA}_{ij} (S)}" />
-        </Button>
+        <Dropdown value={targetValue} 
+                  onChange={(e) => setTargetValue(e.value)} 
+                  options={items}
+                  placeholder="Target" className="expr-input" />
 
-        <Dialog
-          className='dialog'
-          header="Add Rows"
-          visible={visible}
-          onHide={() => {
-            if (!visible) return;
-            setVisible(false);
-          }}
-          footer={dialogFooter}
-        >
+        <Dropdown value={sourceValue} 
+                  onChange={(e) => setSourceValue(e.value)} 
+                  options={items}
+                  placeholder="Source" className="expr-input" />
 
-          <div style={{marginTop: "10px"}}>
-            <InlineMath math={`\\LARGE \\xrightarrow{\\rm{ZA}_{${targetValue +1}${sourceValue +1}} (${scalarFeedback})}`} />
-          </div>
-
-          <table className='dialog_table'><thead>
-          <tr>
-              <th>Target</th>
-              <th>Source</th>
-              <th>Scalar</th>
-          </tr>
-          <tr className="row_container">
-            <td>
-              <SelectButton
-                className="select_btn"
-                value={targetValue}
-                onChange={(e) => setTargetValue(e.value)}
-                options={items}
+      </sub>
+      (
+      <input className="scalar_input" 
+              type='text' 
+              value={scalarInput}
+              onChange={(e) => { handleInput(e.target.value)}}
+              style={{border: scalarFeedback === 'invalid' ? '3px solid red' : '3px solid var(--color5)'}}
               />
-            </td>
+      )
 
-            <td>
-              <SelectButton
-                className="select_btn"
-                value={sourceValue}
-                onChange={(e) => setSourceValue(e.value)}
-                options={items}
-              />
-            </td>
+    </div>
 
-            <td>
-              <input
-                className="scalar_input"
-                type='text'
-                value={scalarInput}
-                onChange={(e) => { handleInput(e.target.value)}}
-              />
-            </td>
-          </tr>
-          </thead></table>
-        </Dialog>
-      </div>
-    );
+    {/* arrow */}
+    <div className="arrow-svg">
+    <svg className="arrow-svg-inner" viewBox="0 0 100 20" preserveAspectRatio="none">
+      <line x1="0" y1="10" x2="95" y2="10" className="arrow-line" />
+      <polygon points="95,5 100,10 95,15" className="arrow-head" />
+    </svg>
+  </div>
+
+  </div>
+
+  <div className='dialog_footer'>
+      <Button label="Cancel" icon="pi pi-times" onClick={() => onClose()}/>
+      <Button label="Apply" icon="pi pi-check" onClick={onConfirm} disabled={scalarFeedback === 'invalid'}/>
+  </div>
+
+
+</>
+);
 }
 
-function SwitchButton({ deactivate , matrix, setMatrix}) {
+function SwitchInline({ matrix, setMatrix, onClose }) {
   const dimension = matrix.length;
   const items = Array.from({ length: dimension }, (_, i) => ({
     label: `${i+1}`, value: i }
   ));
 
-   const [visible, setVisible] = useState(false);
    const [sourceValue, setSourceValue] = useState(items[0].value)
    const [targetValue, setTargetValue] = useState(items[0].value);
 
    function onConfirm() {
     const newMatrix = SwitchRows(matrix, sourceValue, targetValue);
     setMatrix(newMatrix);
-    setVisible(false);
+    onClose();
    }
 
-   const dialogFooter = (
-     <div className='dialog_footer'>
-       <Button label="Apply"  icon="pi pi-check" onClick={onConfirm} />
-     </div>
-   );
+    return(
+      <>
+  <div className="arrow-wrapper">
+    <div className="arrow-expression">
+      ZV
+      <sub>
+        <Dropdown value={targetValue} 
+                  onChange={(e) => setTargetValue(e.value)} 
+                  options={items}
+                  placeholder="Row" className="expr-input" />
 
-   return (
-     <div>
-       <Button
-         disabled={deactivate}
-         onClick={() => {
-           setVisible(true);
-         }}
-       >
-         <InlineMath math="\xrightarrow{\rm{ZV}_{ij}}" />
-       </Button>
+        <Dropdown value={sourceValue} 
+                  onChange={(e) => setSourceValue(e.value)} 
+                  options={items}
+                  placeholder="Row" className="expr-input" />
+      </sub>
 
-       <Dialog
-         className='dialog'
-         header="Switch Rows"
-         visible={visible}
-         onHide={() => {
-           if (!visible) return;
-           setVisible(false);
-         }}
-         footer={dialogFooter}
-       >
+    </div>
 
-          <div style={{marginTop: "10px"}}>
-            <InlineMath math={`\\LARGE \\xrightarrow{\\rm{ZV}_{${sourceValue +1}${targetValue +1}}}`} />
-          </div>
+    {/* arrow */}
+    <div className="arrow-svg">
+    <svg className="arrow-svg-inner" viewBox="0 0 100 20" preserveAspectRatio="none">
+      <line x1="0" y1="10" x2="95" y2="10" className="arrow-line" />
+      <polygon points="95,5 100,10 95,15" className="arrow-head" />
+    </svg>
+  </div>
 
-          <table className='dialog_table'><thead>
-            <tr>
-              <th>Row i</th>
-              <th>Row j</th>
-            </tr>
-           <tr className="row_container">
-            <td>
-             <SelectButton
-               className="select_btn"
-               value={sourceValue}
-               onChange={(e) => setSourceValue(e.value)}
-               options={items}
-             />
-            </td>
-           <td>
-             <SelectButton
-               className="select_btn"
-               value={targetValue}
-               onChange={(e) => setTargetValue(e.value)}
-               options={items}
-             />
-           </td>
-           </tr>
-          </thead></table>
-       </Dialog>
-     </div>
-   );
+  </div>
+
+  <div className='dialog_footer'>
+      <Button label="Cancel" icon="pi pi-times" onClick={() => onClose()}/>
+      <Button label="Apply" icon="pi pi-check" onClick={onConfirm} />
+  </div>
+
+
+</>
+);
 }
